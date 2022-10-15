b8228796561d78a831bd7d6d519f890b2e342570
sarthak395 <sarthak@Sarthaks-MacBook-Air.local>
2022-10-15 16:17:26 +0530

    Init Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<div>
+  <Navbar />
+  <Component {...pageProps} />
+  <Footer />
+  </div>)
 }
 
 export default MyApp
